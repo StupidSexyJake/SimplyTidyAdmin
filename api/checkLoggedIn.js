fd6ca6aff10c1f2d55ba8e35510b5250c117ed71
unknown <jake@goldcoastmaids.com.au>
2019-03-20 13:56:45 +0700

    fix: fix cookies issue

--- api/checkLoggedIn.js
+++ api/checkLoggedIn.js
@@ -1,9 +1,4 @@
-import cookie from 'cookie'
-import { GET_ME, REFRESH_AUTH_TOKEN } from './graphql'
-
-function parseCookies(req, options = {}) {
-    return cookie.parse(req ? req.headers.cookie || '' : document.cookie, options)
-}
+import { GET_ME } from './graphql'
 
 export default apolloClient =>
     apolloClient
@@ -11,21 +6,8 @@ export default apolloClient =>
             query: GET_ME
         })
         .then(({ data }) => {
-            console.log('hit get me')
-            console.log(data)
             return { loggedInUser: data }
         })
         .catch((err) => {
-            console.log('catch get me')
-            // Get tokens from cookies
-            // const tokens = parseCookies()
-            // console.log(tokens)
-            // apolloClient.mutate({
-            //     mutation: REFRESH_AUTH_TOKEN,
-            //     variables: {
-            //                             token: oldHeaders['x-token'],
-            //                             refreshToken: oldHeaders['x-token-refresh']
-            //                         },
-            // })
             return { loggedInUser: {} }
         })
