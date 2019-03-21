959a000059382afdff98f1e9ce9e7277311ff20f
unknown <jake@goldcoastmaids.com.au>
2019-03-21 16:46:24 +0700

    fix: try getting refresh tokens working on server side yet again

--- api/init-apollo.js
+++ api/init-apollo.js
@@ -90,7 +90,7 @@ function create(initialState, { getTokens }) {
         connectToDevTools: process.browser,
         ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
         link: ApolloLink.from([
-            // authLink,
+            authLink,
             terminatingLink
         ]),
         cache: new InMemoryCache().restore(initialState || {}),
