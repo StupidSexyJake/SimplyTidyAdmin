dea559f39b07dedf7e3a680bf5495201e38fa663
unknown <jake@goldcoastmaids.com.au>
2019-03-22 14:27:12 +0700

    fix: please just work refresh tokens. please.

--- api/init-apollo.js
+++ api/init-apollo.js
@@ -154,7 +154,7 @@ function create(initialState, { getTokens }) {
         connectToDevTools: process.browser,
         ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
         link: ApolloLink.from([
-            errorLink,
+            // errorLink,
             authLink,
             terminatingLink,
         ]),
