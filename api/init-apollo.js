c3b74d806028f86995405b7a410937cff5de9fc9
unknown <jake@goldcoastmaids.com.au>
2019-03-22 14:25:57 +0700

    fix: please just work refresh tokens. please.

--- api/init-apollo.js
+++ api/init-apollo.js
@@ -49,12 +49,40 @@ function create(initialState, { getTokens }) {
 
     // Set headers to include auth and refresh tokens
     const authLink = setContext((_, { headers }) => {
+        let token, refreshToken
+        // Get tokens from cookies
         const tokens = getTokens()
+        // If no auth token provided...
+        if (!tokens.token) {
+            // If refresh token provided...
+            if (tokens.refreshToken) {
+                refreshToken = tokens.refreshToken
+                // Attempt to refreh token
+                client.mutate({
+                    mutation: REFRESH_AUTH_TOKEN,
+                    variables: {
+                        refreshToken
+                    }
+                })
+                    // On successful refresh...
+                    .then(data => {
+                        // Add new auth token to cookies
+                        // document.cookie = cookie.serialize('x-token', data.data.refreshAuthToken.token, {})
+                        // Define token
+                        token = data.data.refreshAuthToken.token
+                    })
+                    // On refresh failure...
+                    .catch(error => {
+                        console.log('failed to refresh token')
+                        console.log(error)
+                    })
+            }
+        }
         return {
             headers: {
                 ...headers,
-                'x-token': tokens['x-token'] ? tokens['x-token'] : '',
-                'x-token-refresh': tokens['x-token-refresh'] ? tokens['x-token-refresh'] : ''
+                'x-token': token,
+                'x-token-refresh': refreshToken
             }
         }
     })
