028fd4ee30011e06ea1b476ad9cab9cdb4e4fe9b
unknown <jake@goldcoastmaids.com.au>
2019-03-24 22:17:08 +0700

    fix: last day of trying to get refresh tokens working

--- api/auth.js
+++ api/auth.js
@@ -9,15 +9,9 @@ import cookie from 'js-cookie'
 import { parseCookies, setCookie, destroyCookie } from 'nookies'
 
 // Check if user is logged in
-export async function checkLoggedIn(ctx) {
-    // Get token from cookies
-    const token = nextCookie(ctx)['x-token']
-
-    // Return no user if no auth token available
-    if (!token) { return { loggedInUser: {} } }
-
+export function checkLoggedIn(ctx) {
     // Verify auth token with server (auth token sent in request headers in ./init-apollo.js)
-    await ctx.apolloClient.query({
+    ctx.apolloClient.query({
         query: GET_ME,
     })
         // Return logged in user on verification success
