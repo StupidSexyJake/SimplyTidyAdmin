c92ee41610d58db34c66ce478700070b83809728
unknown <jake@goldcoastmaids.com.au>
2019-03-24 18:41:29 +0700

    fix: last day of trying to get refresh tokens working

--- api/init-apollo.js
+++ api/init-apollo.js
@@ -75,6 +75,9 @@ function create(initialState, { getTokens }) {
                     if (extensions.code === 'UNAUTHENTICATED' && refreshToken) {
                         // Create a new Observerable
                         return new Observable(async observer => {
+                            console.log('refreshAuthToken')
+                            const x = refreshAuthToken(refreshToken, client)
+                            console.log(x)
                             // Refresh the auth token
                             await refreshAuthToken(refreshToken, client)
                                 // On successful refresh...
