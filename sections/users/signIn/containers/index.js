8a240397c708246dcca792ba1ef8c1ec513304b0
unknown <jake@goldcoastmaids.com.au>
2019-03-25 17:59:12 +0700

    fix: last day of trying to get refresh tokens working

--- sections/users/signIn/containers/index.js
+++ sections/users/signIn/containers/index.js
@@ -29,6 +29,7 @@ function SignInFormContainer({ client }) {
 
     // Handle login value changes
     const onChange = event => {
+        console.log('on change hit')
         console.log(state.user)
         const { name, value } = event.target
         dispatch(handleClick('user', name, value.length))
