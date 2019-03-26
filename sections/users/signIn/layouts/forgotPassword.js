97a191a323398014782c35297b74ce6e600eaccd
unknown <jake@goldcoastmaids.com.au>
2019-03-26 19:05:45 +0700

    feat: add reset password functionality

--- sections/users/signIn/layouts/forgotPassword.js
+++ sections/users/signIn/layouts/forgotPassword.js
@@ -10,40 +10,41 @@ import Slide from '@material-ui/core/Slide'
 
 export default function ForgotPasswordLayout(props) {
     return (
-        <Dialog
-            open={props.dialogState}
-            onClose={props.closeDialog}
-            aria-labelledby='forgotPassword'
-            TransitionComponent={<Slide direction="up" />}
-        >
-            <DialogTitle id='forgotPassword'>
-                Forgot Password
-            </DialogTitle>
-            <form onSubmit={props.onSubmit}>
-                <DialogContent>
-                    <TextField
-                        label='Email or username'
-                        name='login'
-                        id='login'
-                        required
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button
-                        onClick={props.closeDialog}
-                        color='primary'
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        type='submit'
-                        variant='contained'
-                        color='secondary'
-                    >
-                        Reset Password
-                    </Button>
-                </DialogActions>
-            </form>
-        </Dialog>
+        // <Dialog
+        //     open={props.dialogState}
+        //     onClose={props.closeDialog}
+        //     aria-labelledby='forgotPassword'
+        //     TransitionComponent={<Slide direction="up" />}
+        // >
+        //     <DialogTitle id='forgotPassword'>
+        //         Forgot Password
+        //     </DialogTitle>
+        //     <form onSubmit={props.onSubmit}>
+        //         <DialogContent>
+        //             <TextField
+        //                 label='Email or username'
+        //                 name='login'
+        //                 id='login'
+        //                 required
+        //             />
+        //         </DialogContent>
+        //         <DialogActions>
+        //             <Button
+        //                 onClick={props.closeDialog}
+        //                 color='primary'
+        //             >
+        //                 Cancel
+        //             </Button>
+        //             <Button
+        //                 type='submit'
+        //                 variant='contained'
+        //                 color='secondary'
+        //             >
+        //                 Reset Password
+        //             </Button>
+        //         </DialogActions>
+        //     </form>
+        // </Dialog>
+        'test'
     )
 }
