e9cd6067c03f9292ad14309e8053947f704ed90f
unknown <jake@goldcoastmaids.com.au>
2019-04-08 12:21:20 +0700

    fix: add assetprefix to nextjs config to allow multiple apps on single domain

--- api/redirect.js
+++ api/redirect.js
@@ -2,9 +2,9 @@ import Router from 'next/router'
 
 export default (ctx, target) => {
     if (ctx.res) {
-        ctx.res.writeHead(303, { Location: `/admin${target}` })
+        ctx.res.writeHead(303, { Location: `admin${target}` })
         ctx.res.end()
     } else {
-        Router.replace(`/admin${target}`)
+        Router.replace(`admin${target}`)
     }
 }
