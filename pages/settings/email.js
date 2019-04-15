688300e374041d5a6271b6bcc39eccf9be4743e0
unknown <jake@goldcoastmaids.com.au>
2019-04-15 09:44:04 +0700

    feat: add email settings page

--- pages/settings/email.js
+++ pages/settings/email.js
@@ -1,7 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import '../../src/bootstrap'
 import React from 'react'
+// Authentication
+import { checkLoggedIn, redirect } from '../../api/auth'
+// Global page layout
+import Page from '../../sections/global/containers/Page'
 
-export default function TeamIndex() {
+function EmailSettings() {
     return (
-        'Test'
+        <Page
+            title='Email Settings'
+            metaDescription='Email settings'
+        >
+            Test
+        </Page>
     )
 }
+
+// Before page is rendered...
+EmailSettings.getInitialProps = async ctx => {
+    // Check if user is logged in
+    const { loggedInUser } = await checkLoggedIn(ctx)
+    // If not signed in, redirect to login page
+    if (!loggedInUser.me) { redirect(ctx, '/admin/login') }
+    // Return
+    return { loggedInUser }
+}
+
+export default EmailSettings
